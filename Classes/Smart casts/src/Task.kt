7cfbdcb24df5b9712bd6e418533b1fea1a2e61e5
misuzz <fs9dm3cc8ct1@gmail.com>
2022-11-09 22:20:28 +0900

    clear 2

--- Classes/Smart casts/src/Task.kt
+++ Classes/Smart casts/src/Task.kt
@@ -1,10 +1,18 @@
 fun eval(expr: Expr): Int =
-        when (expr) {
-            is Num -> TODO()
-            is Sum -> TODO()
-            else -> throw IllegalArgumentException("Unknown expression")
-        }
+    when (expr) {
+        is Num -> expr.value
+        is Sum -> eval(expr.left) + eval(expr.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
 
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
+
+
+val left = Num(10)
+val right = Num(20)
+val test= Sum(left, right)
+
+val result = eval(test)
+
